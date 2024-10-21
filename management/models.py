921d7b55b65a0b39d331fe7e25c40539c49aeccf
Nivi <pt@live.in>
2024-10-22 00:56:08 +0530

    correct typos

--- management/models.py
+++ management/models.py
@@ -8,17 +8,17 @@ class Publisher(models.Model):
     name = models.CharField(max_length=200)
 
 class Book(models.Model):
-    bookID = moodels.IntegerField()
+    bookID = models.IntegerField()
     title = models.CharField(max_length=200)
-    authors = model.ManyToManyField(Author)
+    authors = models.ManyToManyField(Author)
     isbn = models.CharField(max_length=10)
     average_rating = models.DecimalField(decimal_places=2, )
     language_code = models.CharField(max_length=3)
-    num_pages = moodels.IntegerField()
-    ratings_count = moodels.IntegerField()
-    text_reviews_count = moodels.IntegerField()
+    num_pages = models.IntegerField()
+    ratings_count = models.IntegerField()
+    text_reviews_count = models.IntegerField()
     publication_date = models.DateTimeField("date published")
-    publisher = model.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
 class Member(models.Model):
     name = models.CharField(max_length=200)
