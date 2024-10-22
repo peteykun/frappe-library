ebbc79f96960869eb596b1c1674f78db58a65c1e
Nivi <pt@live.in>
2024-10-22 17:32:25 +0530

    Add relevant fields to models

--- management/models.py
+++ management/models.py
@@ -19,6 +19,24 @@ class Book(models.Model):
     text_reviews_count = models.IntegerField()
     publication_date = models.DateTimeField("date published")
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=1)
 
 class Member(models.Model):
-    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zip = models.CharField(max_length=200)
+
+class Issuance(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    rent_fee = models.DecimalField(decimal_places=2, max_digits=8)
+    returned = models.BooleanField(default=False)
+
+class Payment(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=8)
